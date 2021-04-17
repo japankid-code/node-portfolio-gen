2daae53dceb7bbddf4784c8e237b4d2bc32b5791
japankid-code <japankid.jake@gmail.com>
2021-04-17 11:16:14 -0500

    add inquirer prompts

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('file written to index.html')
-})
+//     console.log('file written to index.html')
+// })
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
