135438543865ff00a393b0de7817d6e1e832a552
japankid-code <japankid.jake@gmail.com>
2021-04-17 11:01:40 -0500

    implement node

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('file written to index.html')
+})
--- src/page-template.js
+++ src/page-template.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
 const generatePage = (userName, githubName) => {
     return `
     <!DOCTYPE html>
@@ -23,10 +19,6 @@ const generatePage = (userName, githubName) => {
     `;
 };
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
 
-    console.log('file written to index.html')
-})
 
 module.exports = generatePage;
